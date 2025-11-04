efd42414dcc6e427b037c3f0ca6f78f44a00b3ad
alrubpon97 <jrubioponce@byupathway.edu>
2025-11-04 23:26:20 +0000

    Refactor navigation structure and styles for improved accessibility and responsiveness; update JavaScript for menu toggle functionality

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,11 @@ document.getElementById(
 const today = new Date();
 const year = today.getFullYear();
 document.getElementById("currentyear").textContent = year;
+
+const mainnav = document.querySelector("#nav-menu");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener("click", () => {
+  mainnav.classList.toggle("open");
+  hambutton.classList.toggle("open");
+});
